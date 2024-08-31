766737b84db74772d897e06b2f3802cf8394cfae
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-08-31 20:46:38 -0300

    baseUrl

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,5 +1,5 @@
 const localUrl = 'http://localhost:3003'
-const productionUrl = 'https://rodent-moved-morally.ngrok-free.app'
+const productionUrl = 'https://workable-sloth-strangely.ngrok-free.app'
 const profielPicPrefix = 'https://scontent-gru1-2.cdninstagram.com/v/t51.2885-19/'
 
 export default {
