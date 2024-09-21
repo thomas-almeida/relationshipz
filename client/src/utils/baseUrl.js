a5d9240c980d4902e530ca2ae502a061bd15801d
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-09-21 00:36:28 -0300

    add stripe production product ids

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -3,6 +3,11 @@ const productionUrl = 'https://workable-sloth-strangely.ngrok-free.app'
 const profielPicPrefix = 'https://scontent-gru1-2.cdninstagram.com/v/t51.2885-19/'
 
 
+const stripeProdYearlyId = {
+  productId: "prod_Qt5tHoYS9vPOP2",
+  priceId: "price_1Q1JhHLsfqh8wre3xPNQbTUH"
+}
+
 const stripeTestYearlyId = {
   productId: "prod_Qrrg7Rn7gILyIW",
   priceId: "price_1Q07wlLsfqh8wre3G6X4JJ0Z"
@@ -13,4 +18,5 @@ export default {
   productionUrl,
   profielPicPrefix,
   stripeTestYearlyId,
+  stripeProdYearlyId
 }
