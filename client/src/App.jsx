1cd47397b44b3928a57241b581277b000d02a5f0
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-10-02 09:12:22 -0300

    Vercel Analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './views/Home'
 import SignIn from './views/SignIn'
@@ -10,6 +10,7 @@ import TermsAndPolicy from "./views/TermsAndPolicy"
 function App() {
 
   return (
+    <>
     <Router>
       <Routes>
         <Route path='/home' element={<Home />}></Route>
@@ -20,6 +21,8 @@ function App() {
         <Route path='/termos-e-condicoes' element={<TermsAndPolicy />}></Route>
       </Routes>
     </Router>
+    <Analytics/>
+    </>
   )
 }
 
